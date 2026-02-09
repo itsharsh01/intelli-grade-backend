d1a6e62c686792d7d38f95d725f98081e52cb1b7
itsharsh01 <84959458+itsharsh01@users.noreply.github.com>
2026-02-09 23:48:38 +0530

    oriign

--- main.py
+++ main.py
@@ -15,10 +15,13 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
+        "*"
         "http://localhost:5173", 
         "http://127.0.0.1:5173",
         "https://intelligrad-fe.vercel.app",
-        "https://intelligrad-fe.vercel.app/"
+        "https://intelligrad-fe.vercel.app/",
+        "https://intelligrad-fe-git-main-harshgupta-projects-58910481.vercel.app",
+        "https://intelligrad-fe-git-main-harshgupta-projects-58910481.vercel.app/",
     ],
     allow_credentials=True,
     allow_methods=["*"],
